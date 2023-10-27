eb1c233b3f58ef4934e88a9bcded8effc45b4a14
Mariano Bazan <marianobazancc@gmail.com>
2023-10-26 21:05:14 -0300

    Se agrega la solucion del backend

---
+++ backend/API-ARGBroker/Dto/AccionCompraDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API_ARGBroker.Dto
+{
+    public class AccionCompraDto
+    {
+        public int accionId { get; set; }
+        public int cantidad { get; set; }
+        public int usuarioId { get; set; }
+    }
+}
